5b4577e2731092816c60b82486a295656fbfbd03
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-22 09:31:25 +0200

    Use data class

--- src/main/Kroner.kt
+++ src/main/Kroner.kt
@@ -1,6 +1,6 @@
 package main
 
-class Kroner(var amount: Int) {
+data class Kroner(var amount: Int) {
 
     fun times(multiplier: Int): Kroner {
         return Kroner(amount * multiplier)
--- src/main/KronerTest.kt
+++ src/main/KronerTest.kt
@@ -6,12 +6,17 @@ import org.junit.jupiter.api.Test
 internal class KronerTest {
 
     @Test
-    fun testMultiplication() {
+    fun multiplyAmount() {
         val five = Kroner(5)
         var product: Kroner = five.times(2)
         assertEquals(10, product.amount)
 
         product = five.times(3)
         assertEquals(15, product.amount)
+    }
+
+    @Test
+    fun equalityCheck() {
+        assertTrue(Kroner(5) == Kroner(5))
     }
 }
